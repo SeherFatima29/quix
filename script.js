bc53f1d81853a580a6dd3987aa525f80b11ece19
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-09-26 20:49:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var score=0;
+
+var add =prompt('2+2')
+
+if(add == "4"){
+    score++
+}
+var sub=prompt('3-2')
+if(sub == "1"){
+    score++
+}
+var multiply=prompt('3*2')
+if(multiply == '6'){
+       score++
+}
+var adder=prompt('4+4')
+if(adder == '8'){
+     score++
+}
+
+alert("your score is" + score)
